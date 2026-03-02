6583438bf8bcbb569eb6ce6c858bd7c3a9ba9b26
Galhoom <102315329+Galhoom22@users.noreply.github.com>
2026-03-02 22:51:20 +0200

    24. Store new message API endpoint

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\City;
 use App\Http\Controllers\Api\DistrictController;
+use App\Http\Controllers\Api\MessageController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,3 +20,6 @@ Route::get('/cities', CityController::class);
 
 ## Districts Module
 Route::get('/districts', DistrictController::class);
+
+## Messages Module
+Route::post('/messages', MessageController::class);
